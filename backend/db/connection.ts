f6ba608196d0ba555cb1ff96fd0e429580d3b286
Alvish Vekariya <105376243+alvish-vekariya@users.noreply.github.com>
2024-06-17 14:20:29 +0530

    Delete backend directory

--- backend/db/connection.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import config from 'config';
-
-export const connection = async()=>{
-    await mongoose.connect(config.get("MONGODB_URL"));
-}
--- backend/inversfiy config/inversfiy.config.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "inversify";
-import { userService } from "../services";
-
-const container = new Container();
-
-container.bind<userService>(userService).toSelf();
-
-export default container;
